396b715ed2d444c56bf3c0d9b4fa14f0c820c4ec
Julie Park <julie.park01@bbc.co.uk>
2024-12-10 20:54:01 +0000

    Finish Day 3 Part 2

--- src/main/java/com/jpark0224/adventofcode/Day3/Main.java
+++ src/main/java/com/jpark0224/adventofcode/Day3/Main.java
@@ -1,16 +1,16 @@
 package com.jpark0224.adventofcode.Day3;
 
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.List;
 
 public class Main {
     static int totalDistance = 0;
 
     public static void main(String[] args) {
-        Multiplication instructions = new Multiplication();
-        instructions.parseInput();
-        System.out.println(instructions.getInstructions());
-        System.out.println(instructions.multiplyAndAdd());
+        Multiplication instructionsPart2 = new Multiplication();
+
+        instructionsPart2.parseInputPart2();
+        List<String> filteredInstructions = instructionsPart2.filterInstructions();
+        System.out.println(instructionsPart2.multiplyAndAdd(filteredInstructions));
     }
 }
--- src/main/java/com/jpark0224/adventofcode/Day3/Multiplication.java
+++ src/main/java/com/jpark0224/adventofcode/Day3/Multiplication.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,16 +14,23 @@ import java.util.stream.Stream;
 
 public class Multiplication {
     private List<String> instructions;
+    private List<Integer> validIndices;
+    private List<String> filteredInstructions;
 
     public Multiplication() {
         this.instructions = new ArrayList<>();
+        this.filteredInstructions = new ArrayList<>();
     }
     
     public List<String> getInstructions() {
         return this.instructions;
     }
 
-    public Stream<String> useRegex(final String input) {
+    public List<String> getFilteredInstructions() {
+        return this.filteredInstructions;
+    }
+
+    public Stream<String> useRegexPart1(final String input) {
         // Compile regular expression
         String regex = "mul\\(\\d{1,3}+,\\d{1,3}+\\)";
         final Pattern pattern = Pattern.compile(regex);
@@ -31,11 +39,11 @@ public class Multiplication {
                 .map(MatchResult::group);          // Stream<String>
     }
 
-    public void parseInput() {
+    public void parseInputPart1() {
         String filePath = "src/input/Day3.txt";
 
         try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
-            instructions = stream.map(line -> useRegex(line)
+            instructions = stream.map(line -> useRegexPart1(line)
                     // Collect into a list
                     .collect(Collectors.toList()))
                     .flatMap(List::stream)
@@ -46,15 +54,38 @@ public class Multiplication {
         }
     }
 
-    // Part 1
-    public int multiplyAndAdd() {
+    public Stream<String> useRegexPart2(final String input) {
+        // Compile regular expression
+        String regexMul = "mul\\(\\d{1,3}+,\\d{1,3}+\\)|do\\(\\)|don't\\(\\)";
+        final Pattern patternMul = Pattern.compile(regexMul);
+        return patternMul.matcher(input)
+                .results()                       // Stream<MatchResult>
+                .map(MatchResult::group);          // Stream<String>
+    }
+
+    public void parseInputPart2() {
+        String filePath = "src/input/Day3.txt";
+
+        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+            instructions = stream.map(line -> useRegexPart2(line)
+                            // Collect into a list
+                            .collect(Collectors.toList()))
+                    .flatMap(List::stream)
+                    // Collect all lists into a nested list
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int multiplyAndAdd(List<String> list) {
         String regex = "\\d+";
         Pattern pattern = Pattern.compile(regex);
         int sum = 0;
 
-        for (String instructions:instructions) {
+        for (String instruction:list) {
             int result = 1;
-            Matcher matcher = pattern.matcher(instructions);
+            Matcher matcher = pattern.matcher(instruction);
             while (matcher.find()) {
              result *= Integer.parseInt(matcher.group());
             }
@@ -62,5 +93,52 @@ public class Multiplication {
         }
 
         return sum;
+    }
+
+    public List<List<Integer>> getInvalidRanges() {
+        List<List<Integer>> invalidRanges = new ArrayList<>();
+        int dontIndex = -1;
+
+        for (int i = 0; i < instructions.size(); i++) {
+            if (Objects.equals(instructions.get(i), "don't()")) {
+                if (dontIndex == -1) {
+                    dontIndex = i;
+                }
+            } else if ((Objects.equals(instructions.get(i), "do()") || i == instructions.size() - 1) && dontIndex > -1) {
+                List<Integer> pair = new ArrayList<>();
+                pair.add(dontIndex);
+                pair.add(i);
+                invalidRanges.add(pair);
+                dontIndex = -1;
+            }
+        }
+
+        return invalidRanges;
+    }
+
+    public List<String> filterInstructions() {
+        int currentRangeIndex = 0;
+        int rangeStart = -1;
+        int rangeEnd = -1;
+        List<List<Integer>> invalidRanges = getInvalidRanges();
+        for (int i = 0; i < instructions.size(); i++) {
+            if (currentRangeIndex < invalidRanges.size() && i > rangeEnd) {
+                rangeStart = invalidRanges.get(currentRangeIndex).get(0);
+                rangeEnd = invalidRanges.get(currentRangeIndex).get(1);
+                currentRangeIndex++;
+            }
+
+            if (i >= rangeStart && i <= rangeEnd) {
+                continue;
+            }
+
+            if (!Objects.equals(instructions.get(i), "do()")) {
+                filteredInstructions.add(instructions.get(i));
+            }
+        }
+
+
+
+        return filteredInstructions;
     }
 }
